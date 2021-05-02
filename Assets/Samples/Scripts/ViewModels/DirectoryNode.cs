3af701005f8c80fefe78f11cc52d18dcd9a9bd41
Andreas Schmitt <info@vacuumbreather.de>
2021-05-02 08:21:26 +0200

    Add sample specific implementations

---
+++ Assets/Samples/Scripts/ViewModels/DirectoryNode.cs
@@ -0,0 +1,109 @@
+﻿// <copyright file="DirectoryNode.cs" company="VacuumBreather">
+//      Copyright © 2021 VacuumBreather. All rights reserved.
+// </copyright>
+
+namespace Caliburn.Noesis.Samples.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Security;
+    using Cysharp.Threading.Tasks;
+
+    public class DirectoryNode : FileSystemNode
+    {
+        #region Constants and Fields
+
+        public DirectoryInfo DirectoryInfo { get; }
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public DirectoryNode(DirectoryInfo directoryInfo, DirectoryInfo startingDirectory = null)
+            : base(startingDirectory, directoryInfo.Name)
+        {
+            this.DirectoryInfo = directoryInfo;
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        /// <param name="startingDirectory"></param>
+        /// <inheritdoc />
+        protected override UniTask<IEnumerable<FileSystemNode>> GetChildrenAsync(DirectoryInfo startingDirectory)
+        {
+            IEnumerable<FileSystemNode> children;
+
+            try
+            {
+                children = this.DirectoryInfo.EnumerateDirectories()
+                               .Where(directory =>
+                                   (directory.Attributes &
+                                    (FileAttributes.Temporary | FileAttributes.System | FileAttributes.Hidden)) == 0)
+                               .Select(directory => new DirectoryNode(directory, startingDirectory))
+                               .ToList();
+            }
+            catch (SecurityException)
+            {
+                children = Array.Empty<FileSystemNode>();
+
+                // Ignore
+            }
+            catch (UnauthorizedAccessException)
+            {
+                children = Array.Empty<FileSystemNode>();
+
+                // Ignore
+            }
+
+            return UniTask.FromResult(children);
+        }
+
+        /// <inheritdoc />
+        protected override UniTask<IEnumerable<FileInfo>> GetFilesAsync()
+        {
+            IEnumerable<FileInfo> files;
+
+            try
+            {
+                files = this.DirectoryInfo.EnumerateFiles()
+                            .Where(file =>
+                                (file.Attributes &
+                                 (FileAttributes.Temporary | FileAttributes.System | FileAttributes.Hidden)) == 0)
+                            .ToList();
+            }
+            catch (SecurityException)
+            {
+                files = Array.Empty<FileInfo>();
+
+                // Ignore
+            }
+            catch (UnauthorizedAccessException)
+            {
+                files = Array.Empty<FileInfo>();
+
+                // Ignore
+            }
+
+            return UniTask.FromResult(files);
+        }
+
+        /// <inheritdoc />
+        protected override void PotentiallyExpand(DirectoryInfo startingDirectory)
+        {
+            if (DirectoryInfo.IsSameAs(startingDirectory))
+            {
+                IsSelected = true;
+            }
+            if (DirectoryInfo.IsAncestorOf(startingDirectory))
+            {
+                IsExpanded = true;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Samples/Scripts/Views/MainView.xaml.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="MainView.xaml.cs" company="VacuumBreather">
+//      Copyright © 2021 VacuumBreather. All rights reserved.
+// </copyright>
+
+namespace VacuumBreather.Montreal.MapEditor.Views
+{
+    #region Using Directives
+
+#if NOESIS
+    using Noesis;
+
+#else
+    using System.Windows.Controls;
+#endif
+
+    #endregion
+
+    /// <summary>
+    ///     Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : UserControl
+    {
+        #region Constructors and Destructors
+
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+#if NOESIS
+        private void InitializeComponent()
+        {
+            GUI.LoadComponent(this, "Assets/Scripts/MapEditor/Views/MainView.xaml");
+        }
+#endif
+
+        #endregion
+    }
+}
