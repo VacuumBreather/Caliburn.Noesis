e5531ebf0f63b3b30a7791adf9d021c445fbcbca
Andreas Schmitt <info@vacuumbreather.de>
2021-05-05 01:26:21 +0200

    Allow INotifyPropertyChanged and use weak references
    Prior to this change, the event handling tied the lifecycles of the two objects. While this was probably not a big problem since the command usually lives on the view-model it subscribes to, this solution is more general. Now the command can also subscribe more generally to any object implementing INotifyPropertyChanged

--- Assets/Caliburn.Noesis/Extensions/RaisingCommandExtensions.cs
+++ Assets/Caliburn.Noesis/Extensions/RaisingCommandExtensions.cs
@@ -2,6 +2,7 @@
 {
     #region Using Directives
 
+    using System;
     using System.ComponentModel;
     using System.Windows.Input;
 
@@ -19,35 +20,30 @@
         /// </summary>
         /// <param name="command">The command.</param>
         /// <param name="asyncGuard">The <see cref="AsyncGuard" /> to subscribe to.</param>
-        /// <remarks>
-        ///     Calling this causes the command to subscribe to an event on the async guard. This can
-        ///     cause a leak if the guard lives longer than the command. Both instances should ideally have the
-        ///     same lifecycle, i.e. live on the same view-model. Check life cycles before using this.
-        /// </remarks>
         /// <returns>The command.</returns>
         public static IRaisingCommand RaiseWith(this IRaisingCommand command, AsyncGuard asyncGuard)
         {
-            asyncGuard.IsOngoingChanged += (_, __) => command.RaiseCanExecuteChanged();
+            var weakReference = new WeakReference(command);
+            asyncGuard.IsOngoingChanged += (_, __) =>
+                ((IRaisingCommand)weakReference.Target)?.RaiseCanExecuteChanged();
 
             return command;
         }
 
         /// <summary>
         ///     Causes this command to raise its <see cref="ICommand.CanExecuteChanged" /> event whenever
-        ///     the specified <see cref="IScreen" /> raises its
+        ///     the specified <see cref="INotifyPropertyChanged" /> instance raises its
         ///     <see cref="INotifyPropertyChanged.PropertyChanged" /> event.
         /// </summary>
         /// <param name="command">The command.</param>
-        /// <param name="screen">The <see cref="IScreen" /> to watch.</param>
-        /// <remarks>
-        ///     Calling this causes the command to subscribe to an event on the screen. This can cause a
-        ///     leak if the screen lives longer than the command. Both instances should ideally have the same
-        ///     lifecycle, i.e. the command should only subscribe to the screen it lives on.
-        /// </remarks>
+        /// <param name="notify">The <see cref="INotifyPropertyChanged" /> instance to watch.</param>
         /// <returns>The command.</returns>
-        public static IRaisingCommand RaiseWith(this IRaisingCommand command, IScreen screen)
+        public static IRaisingCommand RaiseWith(this IRaisingCommand command,
+                                                INotifyPropertyChanged notify)
         {
-            screen.PropertyChanged += (_, __) => command.RaiseCanExecuteChanged();
+            var weakReference = new WeakReference(command);
+            notify.PropertyChanged += (_, __) =>
+                ((IRaisingCommand)weakReference.Target)?.RaiseCanExecuteChanged();
 
             return command;
         }
