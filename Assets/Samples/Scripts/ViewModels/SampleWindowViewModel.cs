9b2a9218ec379118d03f22b9b5695197b3d20c5a
Andreas Schmitt <info@vacuumbreather.de>
2021-05-05 04:59:28 +0200

    First implementation of hosted windows - not yet draggable

---
+++ Assets/Samples/Scripts/ViewModels/SampleWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Caliburn.Noesis.Samples.ViewModels
+{
+    /// <summary>The view-model for a sample window.</summary>
+    public class SampleWindowViewModel : WindowScreen
+    {
+        #region Constants and Fields
+
+        private static int windowIndex;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <inheritdoc />
+        public SampleWindowViewModel()
+        {
+            DisplayName = $"Window #{windowIndex++}";
+        }
+
+        #endregion
+    }
+}
