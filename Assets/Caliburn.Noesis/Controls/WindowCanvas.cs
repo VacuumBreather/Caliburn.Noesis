5d4235bfa3eac4086f0c4abbae04750ffdf638f7
Andreas Schmitt <info@vacuumbreather.de>
2021-05-07 01:04:19 +0200

    Add WindowHost and WindowCanvas controls

---
+++ Assets/Caliburn.Noesis/Controls/WindowCanvas.cs
@@ -0,0 +1,19 @@
+﻿namespace Caliburn.Noesis.Controls
+{
+    #region Using Directives
+
+#if UNITY_5_5_OR_NEWER
+    using global::Noesis;
+
+#else
+    using System.Windows.Controls;
+#endif
+
+    #endregion
+
+    /// <summary>Used to host windows.</summary>
+    /// <seealso cref="Canvas" />
+    public class WindowCanvas : Canvas
+    {
+    }
+}
