500600c0f300e960e0f33ff2712366c97016d03f
Andreas Schmitt <info@vacuumbreather.de>
2021-05-08 05:24:36 +0200

    Add conductor sample

---
+++ Assets/Samples/ConductorOneActive/Scripts/ConductorOneActiveBootstrapper.cs
@@ -0,0 +1,35 @@
+﻿namespace Caliburn.Noesis.Samples.ConductorOneActive
+{
+    using System;
+    using System.Linq;
+    using Extensions;
+    using ViewModels;
+
+#if UNITY_5_5_OR_NEWER
+    using UnityEngine;
+
+#endif
+
+    /// <summary>Bootstrapper for the Conductor.OneActive sample.</summary>
+#if UNITY_5_5_OR_NEWER
+    [AddComponentMenu("Bootstrappers/Conductor.OneActive Bootstrapper")]
+#endif
+    public class ConductorOneActiveBootstrapper : BootstrapperBase<MainViewModel>
+    {
+        #region Protected Methods
+
+        /// <inheritdoc />
+        protected override MainViewModel GetMainContentViewModel()
+        {
+            var subScreens = GetConfiguration()
+                             .AssemblySource.ViewModelTypes.Where(
+                                 type => type.IsDerivedFromOrImplements(typeof(ISubScreen)))
+                             .Select(Activator.CreateInstance)
+                             .Cast<ISubScreen>();
+
+            return new MainViewModel(subScreens);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Samples/ConductorOneActive/Scripts/ViewModels/BestiaryScreen.cs
@@ -0,0 +1,14 @@
+namespace Caliburn.Noesis.Samples.ConductorOneActive.ViewModels
+{
+    /// <summary>
+    /// The screen listing all the monsters. 
+    /// </summary>
+    public class BestiaryScreen : Screen, ISubScreen
+    {
+        /// <summary>Initializes a new instance of the <see cref="BestiaryScreen" /> class.</summary>
+        public BestiaryScreen()
+        {
+            DisplayName = "Bestiary";
+        }
+    }
+}
