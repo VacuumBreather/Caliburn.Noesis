67f4e383c2bdeac04c5558ed4b3f2793cb25366e
Andreas Schmitt <info@vacuumbreather.de>
2021-05-11 04:24:23 +0200

    Improve MathExpressionCoverter by caching formulas if possible

--- Assets/Caliburn.Noesis/Platform/Converters/MathExpressionConverter.cs
+++ Assets/Caliburn.Noesis/Platform/Converters/MathExpressionConverter.cs
@@ -7,7 +7,9 @@
     using System.Windows.Data;
 #endif
     using System;
+    using System.Collections.Generic;
     using System.Globalization;
+    using System.Linq;
     using Jace;
 
     /// <summary>Converts a mathematical expression into its result.</summary>
@@ -17,8 +19,14 @@
     {
         #region Constants and Fields
 
+        private static readonly IDictionary<string, Func<IDictionary<string, double>, double>>
+            CachedFormulas = new Dictionary<string, Func<IDictionary<string, double>, double>>();
+
         private static readonly CalculationEngine CalculationEngine =
             new CalculationEngine(CultureInfo.InvariantCulture);
+
+        private static readonly Dictionary<string, double> Parameters =
+            new Dictionary<string, double>();
 
         #endregion
 
@@ -30,16 +38,15 @@
                               object parameter,
                               CultureInfo culture)
         {
-            if (!(parameter is string expression) || string.IsNullOrEmpty(expression) || values is null)
+            if (!(parameter is string expression) || string.IsNullOrEmpty(expression) ||
+                values is null)
             {
                 return DependencyProperty.UnsetValue;
             }
 
             try
             {
-                expression = string.Format(CultureInfo.InvariantCulture, expression, values);
-
-                return CalculationEngine.Calculate(expression);
+                return CalculateResult(values, expression);
             }
             catch
             {
@@ -70,9 +77,7 @@
 
             try
             {
-                expression = string.Format(CultureInfo.InvariantCulture, expression, value);
-
-                return CalculationEngine.Calculate(expression);
+                return CalculateResult(new[] { value }, expression);
             }
             catch
             {
@@ -87,6 +92,89 @@
                                   CultureInfo culture)
         {
             throw new NotSupportedException();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static object CalculateResult(object[] values, string expression)
+        {
+            object result;
+            var doubleValues = ToValidDoubles(values).ToArray();
+
+            if (!doubleValues.Any(double.IsNaN))
+            {
+                // Plan A - All values are doubles.
+                Parameters.Clear();
+
+                foreach (var (val, i) in doubleValues.Select((val, i) => (val, i)))
+                {
+                    Parameters.Add($"var{i}", val);
+                }
+
+                var formula = GetFormula(values, expression);
+
+                result = formula.Invoke(Parameters);
+            }
+            else
+            {
+                // Plan B - Values might be interpretable strings.
+                //          Rebuild the expression by directly inserting the values.
+                expression = string.Format(CultureInfo.InvariantCulture, expression, values);
+
+                result = CalculationEngine.Calculate(expression);
+            }
+
+            return result;
+        }
+
+        private static string CreateVariableExpression(IEnumerable<object> values,
+                                                       string expression)
+        {
+            var variableExpression = string.Format(
+                CultureInfo.InvariantCulture,
+                expression,
+                values.Select((_, i) => $"var{i}").Cast<object>().ToArray());
+
+            return variableExpression;
+        }
+
+        private static Func<IDictionary<string, double>, double> GetFormula(
+            IEnumerable<object> values,
+            string expression)
+        {
+            var variableExpression = CreateVariableExpression(values, expression);
+
+            if (!CachedFormulas.TryGetValue(variableExpression, out var formula))
+            {
+                // Build formula and cache it.
+                formula = CalculationEngine.Build(variableExpression);
+                CachedFormulas[variableExpression] = formula;
+            }
+
+            return formula;
+        }
+
+        private static double ToValidDouble(object value)
+        {
+            if (value is double d1)
+            {
+                return d1;
+            }
+
+            return double.TryParse(
+                       value.ToString(),
+                       NumberStyles.Any,
+                       CultureInfo.InvariantCulture,
+                       out var d2)
+                       ? d2
+                       : double.NaN;
+        }
+
+        private static IEnumerable<double> ToValidDoubles(IEnumerable<object> values)
+        {
+            return values.Select(ToValidDouble);
         }
 
         #endregion
