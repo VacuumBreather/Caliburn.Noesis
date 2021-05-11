a76acfa514bc81fb02b58c124b9c8a0509582cc4
Andreas Schmitt <info@vacuumbreather.de>
2021-05-11 10:25:49 +0200

    Finish up CharacterView

--- Assets/Samples/Conductors/Scripts/Views/CharactersView.xaml.cs
+++ Assets/Samples/Conductors/Scripts/Views/CharactersView.xaml.cs
@@ -1,6 +1,5 @@
 ﻿namespace Caliburn.Noesis.Samples.Conductors.Views
 {
-    using Extensions;
 #if UNITY_5_5_OR_NEWER
     using global::Noesis;
 #else
@@ -16,7 +15,7 @@
         /// <summary>Initializes a new instance of the <see cref="CharactersView" /> class.</summary>
         public CharactersView()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         #endregion
---
+++ Assets/Samples/Conductors/Scripts/Views/ListBoxExtensions.cs
@@ -0,0 +1,64 @@
+﻿namespace Caliburn.Noesis.Samples.Conductors.Views
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>Provides attached properties used to display header content in a <see cref="ListBox" />.</summary>
+    public static class ListBoxExtensions
+    {
+        #region Constants and Fields
+
+        /// <summary>The header property.</summary>
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.RegisterAttached(
+                PropertyNameHelper.GetName(nameof(HeaderProperty)),
+                typeof(object),
+                typeof(ListBoxExtensions),
+                new PropertyMetadata(default(object)));
+
+        public static readonly DependencyProperty HeaderTemplateProperty =
+            DependencyProperty.RegisterAttached(
+                PropertyNameHelper.GetName(nameof(HeaderTemplateProperty)),
+                typeof(DataTemplate),
+                typeof(ListBoxExtensions),
+                new PropertyMetadata(default(DataTemplate)));
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>Gets the header content of the specified <see cref="ListBox" />.</summary>
+        /// <param name="listBox">The listBox.</param>
+        /// <returns>The header content.</returns>
+        public static object GetHeader(ListBox listBox)
+        {
+            return listBox.GetValue(HeaderProperty);
+        }
+
+        /// <summary>Gets the template for the header content of the specified <see cref="ListBox" />.</summary>
+        /// <param name="listBox">The list box.</param>
+        /// <returns>The template for the header content.</returns>
+        public static DataTemplate GetHeaderTemplate(ListBox listBox)
+        {
+            return (DataTemplate)listBox.GetValue(HeaderTemplateProperty);
+        }
+
+        /// <summary>Sets the header content for the specified <see cref="ListBox" />.</summary>
+        /// <param name="listBox">The listBox.</param>
+        /// <param name="value">The header content.</param>
+        public static void SetHeader(ListBox listBox, object value)
+        {
+            listBox.SetValue(HeaderProperty, value);
+        }
+
+        /// <summary>Sets the template for the header content of the specified <see cref="ListBox" />.</summary>
+        /// <param name="listBox">The list box.</param>
+        /// <param name="value">The template for the header content.</param>
+        public static void SetHeaderTemplate(ListBox listBox, DataTemplate value)
+        {
+            listBox.SetValue(HeaderTemplateProperty, value);
+        }
+
+        #endregion
+    }
+}
