9844ca3985123b83116fa4c0fd92fc651f6bc02d
Andreas Schmitt <info@vacuumbreather.de>
2021-05-31 02:38:56 +0200

    Remove logger configuration from HelloWorld to keep it simple

--- Assets/Samples/HelloWorld/Scripts/HelloWorldBootstrapper.cs
+++ Assets/Samples/HelloWorld/Scripts/HelloWorldBootstrapper.cs
@@ -1,6 +1,5 @@
 namespace Caliburn.Noesis.Samples.HelloWorld
 {
-    using Microsoft.Extensions.Logging;
     using ViewModels;
 #if UNITY_5_5_OR_NEWER
     using UnityEngine;
@@ -13,18 +12,5 @@ namespace Caliburn.Noesis.Samples.HelloWorld
 #endif
     public class HelloWorldBootstrapper : BootstrapperBase<MainViewModel>
     {
-        #region Protected Methods
-
-        /// <summary>Override this to add custom behavior on initialization.</summary>
-        protected override void OnInitialize()
-        {
-#if UNITY_5_5_OR_NEWER
-            LogManager.SetLoggerFactory(new DebugLoggerFactory(this, LogLevel.Debug));
-#else
-            LogManager.SetLoggerFactory(new DebugLoggerFactory(LogLevel.Trace));
-#endif
-        }
-
-        #endregion
     }
 }
