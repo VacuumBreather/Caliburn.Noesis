0bad8b88ab91cf01f46ae09e75f983a5f136bd64
Andreas Schmitt <info@vacuumbreather.de>
2021-06-03 20:43:43 +0200

    Make view caching work for multiple locations by assigning location IDs
    Prior to this change, a view could only be used in one location when bound to the same view-model.
    
    This change allows the use in multiple locations by caching them by location ID.

---
+++ Assets/Caliburn.Noesis/Platform/IViewAware.cs
@@ -0,0 +1,30 @@
+﻿namespace Caliburn.Noesis
+{
+    using System;
+    using JetBrains.Annotations;
+    using ViewAttachedEventHandler = System.EventHandler<ViewAttachedEventArgs>;
+#if UNITY_5_5_OR_NEWER
+    using global::Noesis;
+
+#else
+    using System.Windows;
+#endif
+
+    /// <summary>Defines a view-model type which is aware of its view.</summary>
+    [PublicAPI]
+    public interface IViewAware
+    {
+        /// <summary>Raised when a view is attached.</summary>
+        event ViewAttachedEventHandler ViewAttached;
+
+        /// <summary>Attaches a view to this instance.</summary>
+        /// <param name="view">The view.</param>
+        /// <param name="context">The ID of the context in which the view appears.</param>
+        void AttachView(UIElement view, Guid context);
+
+        /// <summary>Gets a view previously attached to this instance.</summary>
+        /// <param name="context">The ID of the context in which the view appears.</param>
+        /// <returns>The attached view, or <c>null</c> if no view has been attached for the specified context.</returns>
+        UIElement GetView(Guid context);
+    }
+}
