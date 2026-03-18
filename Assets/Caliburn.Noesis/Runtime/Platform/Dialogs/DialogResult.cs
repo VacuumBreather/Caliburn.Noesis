87cee946cff2e11c6bebfd21cdaabb0fb4f9a4de
Andreas Schmitt <andreas@vacuumbreather.de>
2026-03-18 20:44:52 +0100

    Extend possible dialog results

--- Assets/Caliburn.Noesis/Runtime/Platform/Dialogs/DialogResult.cs
+++ Assets/Caliburn.Noesis/Runtime/Platform/Dialogs/DialogResult.cs
@@ -1,33 +1,58 @@
 namespace Caliburn.Noesis
 {
     /// <summary>
-    /// Specifies identifiers to indicate the return value of a dialog.
+    ///     Specifies identifiers to indicate the return value of a dialog.
     /// </summary>
     public enum DialogResult
     {
         /// <summary>
-        /// Nothing is returned from the dialog. This means that the modal dialog continues running.
+        /// The dialog returns no result.
         /// </summary>
         None = 0,
 
         /// <summary>
-        /// The dialog return value is Cancel (usually sent from a button labeled Cancel).
+        /// The result value of the message box is OK (usually sent from a button labeled OK).
         /// </summary>
-        Cancel = 1,
+        Ok = 1,
 
         /// <summary>
-        /// The dialog return value is OK (usually sent from a button labeled OK).
+        /// The result value of the message box is Cancel (usually sent from a button labeled Cancel).
         /// </summary>
-        Ok = 2,
+        Cancel = 2,
+
+        /// <summary>
+        /// The result value of the message box is Abort (usually sent from a button labeled Abort).
+        /// </summary>
+        Abort = 3,
+
+        /// <summary>
+        /// The result value of the message box is Retry (usually sent from a button labeled Retry).
+        /// </summary>
+        Retry = 4,
+
+        /// <summary>
+        /// The result value of the message box is Ignore (usually sent from a button labeled Ignore).
+        /// </summary>
+        Ignore = 5,
 
         /// <summary>
         /// The dialog return value is Yes (usually sent from a button labeled Yes).
         /// </summary>
-        Yes = 3,
+        Yes = 6,
 
         /// <summary>
         /// The dialog return value is No (usually sent from a button labeled No).
         /// </summary>
-        No = 4,
+        No = 7,
+
+        /// <summary>
+        /// The result value of the message box is TryAgain (usually sent from a button labeled Try Again).
+        /// </summary>
+        TryAgain = 8,
+
+        /// <summary>
+        /// The result value of the message box is Continue (usually sent from a button labeled Continue).
+        /// </summary>
+        Continue = 9
     }
 }
--- Assets/Caliburn.Noesis/Runtime/Platform/Dialogs/DialogResults.cs
+++ Assets/Caliburn.Noesis/Runtime/Platform/Dialogs/DialogResults.cs
@@ -11,25 +11,52 @@ namespace Caliburn.Noesis
         /// <summary>No possible return value is defined.</summary>
         None = 0,
 
-        /// <summary>The dialog can return Cancel.</summary>
-        Cancel = 0b0001,
-
         /// <summary>The dialog can return Ok.</summary>
-        Ok = 0b0010,
+        Ok = 0b0000_0000_0001,
+
+        /// <summary>The dialog can return Cancel.</summary>
+        Cancel = 0b0000_0000_0010,
+
+        /// <summary>The dialog can return Abort.</summary>
+        Abort = 0b0000_0000_0100,
+
+        /// <summary>The dialog can return Retry.</summary>
+        Retry = 0b0000_0000_1000,
+
+        /// <summary>The dialog can return Ignore.</summary>
+        Ignore = 0b0000_0001_0000,
+
+        /// <summary>The dialog can return Yes.</summary>
+        Yes = 0b0000_0010_0000,
+
+        /// <summary>The dialog can return No.</summary>
+        No = 0b0000_0100_0000,
+
+        /// <summary>The dialog can return Ignore.</summary>
+        TryAgain = 0b0000_1000_0000,
+
+        /// <summary>The dialog can return Ignore.</summary>
+        Continue = 0b0001_0000_0000,
 
         /// <summary>The dialog can return either Ok or Cancel.</summary>
         OkCancel = Ok | Cancel,
 
-        /// <summary>The dialog can return Yes.</summary>
-        Yes = 0b0100,
-
-        /// <summary>The dialog can return No.</summary>
-        No = 0b1000,
-
         /// <summary>The dialog can return either Yes or No.</summary>
         YesNo = Yes | No,
 
-        /// <summary>The dialog can return either Yes, No or Cancel.</summary>
+        /// <summary>The dialog can return either Yes, No, or Cancel.</summary>
         YesNoCancel = Yes | No | Cancel,
+
+        /// <summary>The dialog can return either Retry or Abort.</summary>
+        RetryAbort = Retry | Abort,
+
+        /// <summary>The dialog can return either Retry, Abort, or Continue.</summary>
+        RetryAbortContinue = Retry | Abort | Continue,
+
+        /// <summary>The dialog can return either TryAgain or Abort.</summary>
+        TryAgainAbort = TryAgain | Abort,
+
+        /// <summary>The dialog can return either TryAgain, Abort, or Ignore.</summary>
+        TryAgainAbortIgnore = TryAgain | Abort | Ignore,
     }
 }
